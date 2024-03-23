69c73e3351bf2d742b4f9cdd53d821a4edf4b2a6
Alex Helder <helder.huancara@gmail.com>
2024-03-23 10:49:34 -0500

    Ejercicio resuelto dia 1

---
+++ ejercicios/dia-1.js
@@ -0,0 +1,10 @@
+//ejercicio 1
+let operation = (13 - 27) * 43 - 12 * 8 + (78 - 56) - -338 * 2;
+console.log(operation);
+
+let secondOperation = (23 + 5 * (2 - 3) > 5 * 10 && true) || false || 10 >= 3;
+console.log(secondOperation);
+
+//ejercicio 2
+console.log(typeof secondOperation);
+console.log(typeof operation);
